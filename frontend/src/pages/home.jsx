0242e996bad9ecc366efbe6794c4971ddbf900ae
Louzada27 <20221SI005@cefetes.br>
2025-06-06 08:44:54 -0300

    Atualizaçõ BD

---
+++ frontend/src/pages/home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+ function Home() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>Bem-vindo ao SCV</h1>
+      <p>Este é o frontend básico da Home.</p>
+      <ul>
+        <li>Funcionalidade 1</li>
+        <li>Funcionalidade 2</li>
+        <li>Funcionalidade 3</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Home;
