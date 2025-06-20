3ae2e885915c9b8b82b753d3ecd7ff20d983d4ef
Louzada27 <gabrielbancon@gmail.com>
2025-06-20 18:50:32 -0300

    Atualizações

--- src/server.js
+++ src/server.js
@@ -3,7 +3,6 @@ import cors from 'cors';
 import { sequelize, models } from './models/index.js';
 import routes from './routes.js';
 import errorHandler from '../src/_middleware/error-handler.js';
-import { seedDatabase } from './config/database-seed.js';
 
 const app = express();
 const port = process.env.PORT || 3333;
@@ -12,14 +11,13 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-sequelize.sync({ force:true, alter: true }).catch((error) => {
+sequelize.sync({ force: true, alter: true }).catch((error) => {
     console.error('Erro ao sincronizar tabelas:', error);
 });
 
 app.use(errorHandler); // Manipulador de erro global (error handler)
-app.listen(port, async () => {
+app.listen(port, async() => {
     console.log(`Servidor rodando na porta ${port}`);
-    await seedDatabase(); // Moved seedDatabase call here
 });
 
 // Ao iniciar o sistema, é necessário que pelo menos um endereço e um bairro estejam 
