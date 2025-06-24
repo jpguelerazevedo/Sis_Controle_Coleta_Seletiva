be7c2672a016c4aceaef855a63e8138b08cd5d97
Gabriel Louzada da Silva <131915775+Louzada27@users.noreply.github.com>
2025-06-24 09:38:36 -0300

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/scv/',  // nome do seu repo com barras
   plugins: [react()],
-})
+});
