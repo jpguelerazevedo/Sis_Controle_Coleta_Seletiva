f9c8d619c5bb0b703380b4b775c8d9e5f04743fc
jpguelerazevedo <jpguelerazevedo@gmail.com>
2025-06-26 20:12:57 -0300

    concerto erro: force true

--- src/config/database-connection.js
+++ src/config/database-connection.js
@@ -3,10 +3,9 @@ import { sequelize, models } from '../models/index.js';
 const syncDatabase = async () => {
     try {
         // Sincroniza os modelos com o banco
-        await sequelize.sync({ force: true }); 
+        await sequelize.sync();
         // ⚠️ Atenção: force: true apaga e recria as tabelas. 
         // Use { alter: true } se quiser atualizar sem apagar dados.
-
         console.log('Database synchronized successfully');
     } catch (error) {
         console.error('Error synchronizing database:', error);
--- src/server.js
+++ src/server.js
@@ -11,18 +11,18 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-sequelize.sync({ force: true, alter: true }).catch((error) => {
+sequelize.sync().catch((error) => {
     console.error('Erro ao sincronizar tabelas:', error);
 });
 
 app.use(errorHandler); // Manipulador de erro global (error handler)
-app.listen(port, async() => {
+app.listen(port, async () => {
     console.log(`Servidor rodando na porta ${port}`);
 });
 
-// Ao iniciar o sistema, é necessário que pelo menos um endereço e um bairro estejam 
-// cadastrados, pois o cliente possui uma associação direta com o endereço, que por 
+// Ao iniciar o sistema, é necessário que pelo menos um endereço e um bairro estejam
+// cadastrados, pois o cliente possui uma associação direta com o endereço, que por
 // sua vez está vinculado a um bairro.
-// Como não foi implementado um cadastro específico para endereços e bairros, tornou - se 
-// necessário realizar esses cadastros iniciais manualmente para garantir o funcionamento correto 
+// Como não foi implementado um cadastro específico para endereços e bairros, tornou - se
+// necessário realizar esses cadastros iniciais manualmente para garantir o funcionamento correto
 // do relacionamento entre as entidades.
