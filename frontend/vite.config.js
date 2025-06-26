65db71b93473c204e01dc52af03a883e5092361f
João Paulo Azevedo <145774523+jpguelerazevedo@users.noreply.github.com>
2025-06-26 12:03:45 -0300

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
-  base: '/scv/',  // nome do seu repo com barras
+  base: '/Sis_Controle_Coleta_Seletiva/',  // nome do seu repo com barras
   plugins: [react()],
 });
