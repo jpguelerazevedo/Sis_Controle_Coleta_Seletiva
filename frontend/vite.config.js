f7b67ad634a22ea7cd7dc555075deaf676467664
jpguelerazevedo <jpguelerazevedo@gmail.com>
2025-06-28 10:02:27 -0300

    alterando vite.config / pages

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
-  base: '/Sis_Controle_Coleta_Seletiva/',  // nome do seu repo com barras
+  base: '/selective-collection-control/',  // nome do seu repo com barras
   plugins: [react()],
 });
